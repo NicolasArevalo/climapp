f48d5ee88b25ff4a5349964d812b9e1ba471a3f2
Nicolás Arévalo <inag.458@gmail.com>
2022-03-27 15:33:51 -0500

    Changing the snowpack.config.js baseURL

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://github.com/nicolasarevalo/climapp',
+    baseUrl: 'https://nicolasarevalo.github.com/climapp',
   },
 }
